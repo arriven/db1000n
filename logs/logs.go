16d28c43047b08b688750dda8a9c0cd8221f1003
Oleksii Volkov <alexey.wild88@gmail.com>
2022-02-27 15:14:15 +0200

    small fix

--- logs/logs.go
+++ logs/logs.go
@@ -10,9 +10,9 @@ type Logs struct {
 }
 
 func (l Logs) Debug(format string, a ...interface{}) {
-	log.Printf("[DEBUG] %s", fmt.Sprintf(format, a))
+	log.Printf("[DEBUG] %s", fmt.Sprintf(format, a...))
 }
 
 func (l Logs) Error(format string, a ...interface{}) {
-	log.Printf("[ERROR] %s", fmt.Sprintf(format, a))
+	log.Printf("[ERROR] %s", fmt.Sprintf(format, a...))
 }
