75494cc0b2afca9b86ea24715a69fd53d36cb022
k0xxx <androidk0xxx@gmail.com>
2022-02-27 17:50:11 +0200

    Remove env

--- main.go
+++ main.go
@@ -28,12 +28,6 @@ import (
 	"db1000n/logs"
 )
 
-// Init environment
-func initEnvironment() {
-	os.Setenv("CONFIG_PATH", "https://raw.githubusercontent.com/db1000n-coordinators/LoadTestConfig/main/config.json")
-	//os.Setenv("CONFIG_PATH", "./config.json")
-}
-
 // JobArgs comment for linter
 type JobArgs = json.RawMessage
 
@@ -338,9 +332,6 @@ func fetchConfig(configPath string) (*Config, error) {
 }
 
 func main() {
-	// Init env variables
-	initEnvironment()
-
 	var configPath string
 	var refreshTimeout time.Duration
 	var logLevel logs.Level
