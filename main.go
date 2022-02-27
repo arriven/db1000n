23a605ee4651e1a18495426d87ea89a1eec6c947
Zherebko Dmitry <kraken@live.ru>
2022-02-27 19:44:22 +0200

    fix go mod

--- main.go
+++ main.go
@@ -21,13 +21,10 @@ import (
 	"github.com/corpix/uarand"
 	"github.com/google/uuid"
 
-	"db1000n/slowloris"
-
+	"github.com/Arriven/db1000n/logs"
+	"github.com/Arriven/db1000n/metrics"
+	"github.com/Arriven/db1000n/slowloris"
 	"github.com/Arriven/db1000n/synfloodraw"
-
-	"db1000n/logs"
-	"db1000n/metrics"
-	"db1000n/synfloodraw"
 )
 
 // JobArgs comment for linter
