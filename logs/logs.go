730407214c465662091c463dd1f87e38caa44dbd
arriven <bogdannuch@gmail.com>
2022-02-27 15:59:29 +0200

    Added log levels to the cmdline

--- logs/logs.go
+++ logs/logs.go
@@ -5,14 +5,25 @@ import (
 	"log"
 )
 
-type Logs struct {
+type Level = int
 
+const (
+	Debug Level = iota
+	Info
+	Warning
+	Error
+)
+
+type Logger struct {
+	Level Level
 }
 
-func (l Logs) Debug(format string, a ...interface{}) {
-	log.Printf("[DEBUG] %s", fmt.Sprintf(format, a...))
+func (l Logger) Debug(format string, a ...interface{}) {
+	if l.Level <= Debug {
+		log.Printf("[DEBUG] %s", fmt.Sprintf(format, a...))
+	}
 }
 
-func (l Logs) Error(format string, a ...interface{}) {
+func (l Logger) Error(format string, a ...interface{}) {
 	log.Printf("[ERROR] %s", fmt.Sprintf(format, a...))
 }
