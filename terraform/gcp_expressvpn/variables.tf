747472b5b8229a2a9e62b6c0fa46107faa918fb4
Ivan Chornyi <paalomnik@gmail.com>
2022-03-03 22:56:02 +0200

    terraform initial

---
+++ terraform/gcp_expressvpn/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "GCP project id"
+}
+
+variable "expressvpn_key" {
+  description = "expressvpn activation key"
+}
+
+variable "vpn_location" {
+  description = "vpn server location to use initially"
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  default = "n1-standard-1"
+}
+
+variable "machine_location" {
+  description = "machine location"
+  default = "us-central1-a"
+}
