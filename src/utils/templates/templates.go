78c6d04273cb0fdf538aeebd8e1a2153eb606462
arfgdev <arforgethedev@gmail.com>
2022-03-05 22:18:16 +0200

    added get proxies by url function and proxies url argument

--- src/utils/templates/templates.go
+++ src/utils/templates/templates.go
@@ -15,25 +15,32 @@ import (
 	"github.com/Arriven/db1000n/src/packetgen"
 )
 
+var proxiesURL = "https://raw.githubusercontent.com/Arriven/db1000n/main/proxylist.json"
+
 func getProxylistURL() string {
-	return "https://raw.githubusercontent.com/Arriven/db1000n/main/proxylist.json"
+	return proxiesURL
+}
+
+func SetProxiesUrl(url string) {
+	proxiesURL = url
 }
 
 func getProxylist() (urls []string) {
-	resp, err := http.Get(getProxylistURL())
+	return getProxylistByUrl(getProxylistURL())
+}
+
+func getProxylistByUrl(url string) (urls []string) {
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil
 	}
-
 	defer resp.Body.Close()
-
-	if err = json.NewDecoder(resp.Body).Decode(&urls); err != nil {
+	err = json.NewDecoder(resp.Body).Decode(&urls)
+	if err != nil {
 		return nil
 	}
-
 	return urls
 }
-
 func getURLContent(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -56,25 +63,27 @@ func randomUUID() string {
 
 func Execute(input string) string {
 	funcMap := template.FuncMap{
-		"random_uuid":     randomUUID,
-		"random_int_n":    rand.Intn,
-		"random_int":      rand.Int,
-		"random_payload":  packetgen.RandomPayload,
-		"random_ip":       packetgen.RandomIP,
-		"random_port":     packetgen.RandomPort,
-		"random_mac_addr": packetgen.RandomMacAddr,
-		"local_ip":        packetgen.LocalIP,
-		"local_mac_addr":  packetgen.LocalMacAddres,
-		"base64_encode":   base64.StdEncoding.EncodeToString,
-		"base64_decode":   base64.StdEncoding.DecodeString,
-		"json_encode":     json.Marshal,
-		"json_decode":     json.Unmarshal,
-		"get_url":         getURLContent,
-		"proxylist_url":   getProxylistURL,
-		"get_proxylist":   getProxylist,
+		"random_uuid":          randomUUID,
+		"random_int_n":         rand.Intn,
+		"random_int":           rand.Int,
+		"random_payload":       packetgen.RandomPayload,
+		"random_ip":            packetgen.RandomIP,
+		"random_port":          packetgen.RandomPort,
+		"random_mac_addr":      packetgen.RandomMacAddr,
+		"local_ip":             packetgen.LocalIP,
+		"local_mac_addr":       packetgen.LocalMacAddres,
+		"base64_encode":        base64.StdEncoding.EncodeToString,
+		"base64_decode":        base64.StdEncoding.DecodeString,
+		"json_encode":          json.Marshal,
+		"json_decode":          json.Unmarshal,
+		"get_url":              getURLContent,
+		"proxylist_url":        getProxylistURL,
+		"get_proxylist":        getProxylist,
+		"get_proxylist_by_url": getProxylistByUrl,
 	}
 
 	// TODO: consider adding ability to populate custom data
+	input = strings.Replace(input, "\\", "", -1)
 	tmpl, err := template.New("test").Funcs(funcMap).Parse(input)
 	if err != nil {
 		logs.Default.Warning("error parsing template: %v", err)
