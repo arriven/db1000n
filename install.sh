59721b5a63a246eaf1a6988bd1ff3725c2433b54
Amet Umerov <admin@amet13.name>
2022-03-06 10:15:21 +0200

    [Install script] Fix md5sum for macOS, test install script on CI, fail script in case of error (#149)
    * Fix some misspells and misslinks
    
    * Add DO docs
    
    * Remove redundant layers
    
    * Compact dockerfile
    
    * Upgrade install script
    
    * Fix suffix
    
    * temp: debug mode
    
    * Disable debug

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+
+set -euo pipefail
+
 REPO="Arriven/db1000n"
 INSTALL_OS="unknown"
+
 case "$OSTYPE" in
   solaris*) INSTALL_OS="solaris" ;;
   darwin*)  INSTALL_OS="darwin" ;; 
@@ -26,21 +30,33 @@ esac
 
 INSTALL_VERSION="${INSTALL_OS}-${INSTALL_ARCH}"
 
-curl https://api.github.com/repos/${REPO}/releases/latest | grep "${INSTALL_VERSION}" | grep -Eo 'https://[^\"]*' | xargs -n 1 curl -L -O
+echo "Downloading an archive..."
+curl -s https://api.github.com/repos/${REPO}/releases/latest | grep "${INSTALL_VERSION}" | grep -Eo 'https://[^\"]*' | xargs -n 1 curl -s -L -O
 
 INSTALL_TAG=$(curl --silent "https://api.github.com/repos/${REPO}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 ARCHIVE="db1000n-${INSTALL_TAG}-${INSTALL_VERSION}.tar.gz"
 
-if ! command -v md5sum &> /dev/null
+if [ "${INSTALL_OS}" = "darwin" ]
 then
-    echo "Warning: md5sum is not be found. Could not check arhive integrity. Please be cautious when launching the executable."
+  MD5_BINARY="md5"
+  MD5_SUFFIX="-q"
 else
-    cat ${ARCHIVE} | md5sum | awk '{ print $1 }' > md5sum.txt
-      if ! cmp --silent "${ARCHIVE}.md5" "md5sum.txt"; then
-      echo "md5sum for ${ARCHIVE} failed. Please check the md5sum. File may possibly be corrupted."
-      exit 1
-    fi
+  MD5_BINARY="md5sum"
+  MD5_SUFFIX="-t"
 fi
 
-tar xvf ${ARCHIVE}
+echo "Checking md5 hash..."
+if ! command -v "${MD5_BINARY}" &> /dev/null
+then
+  echo "Warning: md5sum/md5 not found. Could not check archive integrity. Please be careful when launching the executable."
+else
+  "${MD5_BINARY}" "${MD5_SUFFIX}" "${ARCHIVE}" | awk '{ print $1 }' > md5sum.txt
+  if ! cmp --silent "${ARCHIVE}.md5" "md5sum.txt"; then
+    echo "md5sum for ${ARCHIVE} failed. Please check the md5sum. File may possibly be corrupted."
+    exit 1
+  fi
+fi
+
+tar xvf "${ARCHIVE}"
+echo "Successfully installed db1000n"
