cf38f9ade1701d58699b6d8c52b82643a864b28b
sivillakonski <100792803+sivillakonski@users.noreply.github.com>
2022-03-07 01:20:44 -0500

    Refactor TCP-TLS connection to bypass filtering based on SSL fingerprinting (#197)

--- src/dnsblast/dns-blast_test.go
+++ src/dnsblast/dns-blast_test.go
@@ -2,49 +2,98 @@ package dnsblast
 
 import (
 	"context"
-	"net"
-	"strconv"
 	"testing"
 	"time"
 )
 
 func TestBlast(t *testing.T) {
 	const (
-		testDuration = 10 * time.Second
+		testDuration = 5 * time.Second
 
-		testServer = "10.8.0.1"
-		testPort   = DefaultDNSPort
-		testProto  = UDPProtoName
-
-		domainA = "cnn.com"
-		domainB = "yahoo.com"
-		domainC = "foxnews.com"
+		testRootDomain = "example.com"
+		seedDomainA    = "cnn.com"
+		seedDomainB    = "yahoo.com"
+		seedDomainC    = "foxnews.com"
 
 		testIterationDelay = 50 * time.Millisecond
 		testParallelism    = 3
 	)
 
-	var (
-		blastContext, cancel = context.WithTimeout(context.Background(), testDuration)
-		config               = &Config{
-			RootDomain: net.JoinHostPort(testServer, strconv.Itoa(testPort)),
-			Protocol:   testProto,
+	type testCase struct {
+		Name     string
+		Duration time.Duration
+
+		RootDomain      string
+		Protocol        string
+		SeedDomains     []string
+		Delay           time.Duration
+		ParallelQueries int
+	}
+
+	testCases := []testCase{
+		{
+			Name:       "Benchmark over UDP",
+			Duration:   testDuration,
+			RootDomain: testRootDomain,
+			Protocol:   UDPProtoName,
 			SeedDomains: []string{
-				domainA,
-				domainB,
-				domainC,
+				seedDomainA,
+				seedDomainB,
+				seedDomainC,
 			},
 			Delay:           testIterationDelay,
 			ParallelQueries: testParallelism,
-		}
-	)
-	defer cancel()
-
-	err := Start(blastContext, config)
-	if err != nil {
-		t.Errorf("failed to start the blaster: %s", err)
-		return
+		},
+		{
+			Name:       "Benchmark over TCP",
+			Duration:   testDuration,
+			RootDomain: testRootDomain,
+			Protocol:   TCPProtoName,
+			SeedDomains: []string{
+				seedDomainA,
+				seedDomainB,
+				seedDomainC,
+			},
+			Delay:           testIterationDelay,
+			ParallelQueries: testParallelism,
+		},
+		{
+			Name:       "Benchmark over TCP-TLS",
+			Duration:   testDuration,
+			RootDomain: testRootDomain,
+			Protocol:   TCPTLSProtoName,
+			SeedDomains: []string{
+				seedDomainA,
+				seedDomainB,
+				seedDomainC,
+			},
+			Delay:           testIterationDelay,
+			ParallelQueries: testParallelism,
+		},
 	}
 
-	time.Sleep(testDuration + time.Second)
+	for i := range testCases {
+		t.Run(testCases[i].Name, func(tt *testing.T) {
+			config := &Config{
+				RootDomain:      testCases[i].RootDomain,
+				Protocol:        testCases[i].Protocol,
+				SeedDomains:     testCases[i].SeedDomains,
+				Delay:           testCases[i].Delay,
+				ParallelQueries: testCases[i].ParallelQueries,
+			}
+
+			tt.Logf("[%s] benchmark configuration: %+v", testCases[i].Name, config)
+
+			blastContext, cancel := context.WithTimeout(context.Background(), testCases[i].Duration)
+			defer cancel()
+
+			err := Start(blastContext, config)
+			if err != nil {
+				tt.Errorf("failed to start the blaster: %s", err)
+				return
+			}
+
+			time.Sleep(testCases[i].Duration + time.Second)
+		})
+	}
 }
