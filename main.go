0a46e65f95f4c6ae6c50d08c7e11993a5f957b31
Sivilla Konski <sivilla.konski@tutanota.com>
2022-03-13 05:41:12 +0000

    DB1000N | Refactor flags values

--- main.go
+++ main.go
@@ -59,10 +59,10 @@ func main() {
 	configFormat := flag.String("format", utils.GetEnvStringDefault("CONFIG_FORMAT", "json"), "config format")
 	prometheusOn := flag.Bool("prometheus_on", utils.GetEnvBoolDefault("PROMETHEUS_ON", false), "Start metrics exporting via HTTP and pushing to gateways (specified via <prometheus_gateways>)")
 	prometheusPushGateways := flag.String("prometheus_gateways", utils.GetEnvStringDefault("PROMETHEUS_GATEWAYS", ""), "Comma separated list of prometheus push gateways")
-	doAutoUpdate := flag.Bool("enable-self-update", false, "Enable the application automatic updates on the startup")
-	doRestartOnUpdate := flag.Bool("restart-on-update", true, "Allows application to restart upon successful update (ignored if auto-update is disabled)")
-	skipUpdateCheckOnStart := flag.Bool("skip-update-check-on-start", false, "Allows to skip the update check at the startup (usually set automatically by the previous version)")
-	autoUpdateCheckFrequency := flag.Duration("self-update-check-frequency", 24*time.Hour, "How often to run auto-update checks")
+	doAutoUpdate := flag.Bool("enable-self-update", utils.GetEnvBoolDefault("ENABLE_SELF_UPDATE", false), "Enable the application automatic updates on the startup")
+	doRestartOnUpdate := flag.Bool("restart-on-update", utils.GetEnvBoolDefault("RESTART_ON_UPDATE", true), "Allows application to restart upon successful update (ignored if auto-update is disabled)")
+	skipUpdateCheckOnStart := flag.Bool("skip-update-check-on-start", utils.GetEnvBoolDefault("SKIP_UPDATE_CHECK_ON_START", false), "Allows to skip the update check at the startup (usually set automatically by the previous version)")
+	autoUpdateCheckFrequency := flag.Duration("self-update-check-frequency", utils.GetEnvDurationDefault("SELF_UPDATE_CHECK_FREQUENCY", 24*time.Hour), "How often to run auto-update checks")
 
 	flag.Parse()
 
