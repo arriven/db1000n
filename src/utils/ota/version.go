661064ea1a8f5ea8b64c7195946532e90f6bf0fa
arfgdev <arforgethedev@gmail.com>
2022-03-13 16:16:15 +0200

    fix: release script fixed to add correct build flags

--- src/utils/ota/version.go
+++ src/utils/ota/version.go
@@ -2,7 +2,7 @@ package ota
 
 var (
 	// Version is a releae version embedded into the app
-	Version = "v0.0.1"
+	Version = "development"
 	// Repository to check for updates
 	Repository = "Arriven/db1000n" // Could be changed via the ldflags
 )
