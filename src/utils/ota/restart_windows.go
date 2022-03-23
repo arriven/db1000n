936d6dc18052e1297e1b7e9c6ac04e6d13fa8cff
sivillakonski <100792803+sivillakonski@users.noreply.github.com>
2022-03-23 00:52:10 -0400

    DB1000N | Add support for the restart option after the auto-update for the Windows OS (#391)
    * DB1000N | Add support for the restart option after the auto-update for the Windows OS
    
    * DB1000N | Change the Run method to Start to restart the process on the Windows OS

---
+++ src/utils/ota/restart_windows.go
@@ -0,0 +1,46 @@
+//go:build windows
+// +build windows
+
+package ota
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+func Restart(extraArgs ...string) error {
+	execPath, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("failed to locate the executable file: %w", err)
+	}
+
+	// A specific way the `cmd.exe` processes the `start` command: it takes the
+	// first quoted substring as a process name! (https://superuser.com/a/1656458)
+	cmdLine := fmt.Sprintf(`/C start "process" "%s"`, execPath)
+	args := []string{}
+
+	if len(extraArgs) != 0 {
+		args = appendArgIfNotPresent(os.Args[1:], extraArgs)
+	} else {
+		args = os.Args[1:]
+	}
+
+	if len(args) > 0 {
+		cmdLine += " " + strings.Join(args, " ")
+	}
+
+	cmd := exec.Command("cmd.exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{CmdLine: cmdLine}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start the command: %w", err)
+	}
+
+	os.Exit(0)
+
+	return nil
+}
+
---
+++ src/utils/ota/shared.go
@@ -0,0 +1,18 @@
+package ota
+
+func appendArgIfNotPresent(osArgs, extraArgs []string) []string {
+	osArgsMap := make(map[string]interface{}, len(osArgs))
+	for _, osArg := range osArgs {
+		osArgsMap[osArg] = nil
+	}
+
+	acceptedExtraArgs := make([]string, 0)
+
+	for _, extraArg := range extraArgs {
+		if _, isAlreadyOSArg := osArgsMap[extraArg]; !isAlreadyOSArg {
+			acceptedExtraArgs = append(acceptedExtraArgs, extraArg)
+		}
+	}
+
+	return append(osArgs, acceptedExtraArgs...)
+}
