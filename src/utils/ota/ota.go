a92907d5d64525f1cbb8decf3558cc2f241d5798
jdoe <jdoe7865623@tutanota.com>
2022-03-24 13:05:55 +0200

    Make ota.DoAutoUpdate() less cryptic

--- src/utils/ota/ota.go
+++ src/utils/ota/ota.go
@@ -8,38 +8,28 @@ import (
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 )
 
+var (
+	// Version is a release version embedded into the app
+	Version = "v0.0.1"
+	// Repository to check for updates
+	Repository = "Arriven/db1000n" // Could be changed via the ldflags
+)
+
 // DoAutoUpdate updates the app to the latest version.
 func DoAutoUpdate() (updateFound bool, newVersion, changeLog string, err error) {
 	v, err := semver.ParseTolerant(Version)
 	if err != nil {
-		err = fmt.Errorf("binary version validation failed: %w", err)
-
-		return
+		return false, "", "", fmt.Errorf("binary version validation failed: %w", err)
 	}
 
 	latest, err := selfupdate.UpdateSelf(v, Repository)
-	if err != nil {
-		err = fmt.Errorf("binary update failed: %w", err)
 
-		return
+	switch {
+	case err != nil:
+		return false, "", "", fmt.Errorf("binary update failed: %w", err)
+	case latest.Version.Equals(v):
+		return false, "", "", nil
 	}
 
-	if !latest.Version.Equals(v) {
-		updateFound = true
-		newVersion = latest.Version.String()
-		changeLog = latest.ReleaseNotes
-	}
-
-	return
-}
-
-func MockAutoUpdate(shouldUpdateBeFound bool) (updateFound bool, newVersion, changeLog string, err error) {
-	updateFound = shouldUpdateBeFound
-	if updateFound {
-		newVersion = "brand-new-version"
-		changeLog = "something-was-changed"
-		err = nil
-	}
-
-	return
+	return true, latest.Version.String(), latest.ReleaseNotes, nil
 }
