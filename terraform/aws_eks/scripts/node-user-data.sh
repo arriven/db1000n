03d9b18252fc54b1be0a042dcbbe1c65c796be90
Amet13 <admin@amet13.name>
2022-04-01 11:45:46 +0300

    Fix shellcheck

--- terraform/aws_eks/scripts/node-user-data.sh
+++ terraform/aws_eks/scripts/node-user-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-sudo /etc/eks/bootstrap.sh --apiserver-endpoint ${CLUSTER_ENDPOINT} --b64-cluster-ca ${CLUSTER_CA_DATA} ${CLUSTER_NAME}
+sudo /etc/eks/bootstrap.sh --apiserver-endpoint "${CLUSTER_ENDPOINT}" --b64-cluster-ca "${CLUSTER_CA_DATA}" "${CLUSTER_NAME}"
