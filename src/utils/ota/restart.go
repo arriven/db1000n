b4cda7df6c4cd18eb8b41c5678f827b0153fefad
arriven <20084245+Arriven@users.noreply.github.com>
2022-04-22 16:02:38 +0300

    one last import

--- src/utils/ota/restart.go
+++ src/utils/ota/restart.go
@@ -6,8 +6,10 @@ package ota
 import (
 	"fmt"
 	"runtime"
+
+	"go.uber.org/zap"
 )
 
-func restart(extraArgs ...string) error {
+func restart(logger *zap.Logger, extraArgs ...string) error {
 	return fmt.Errorf("restart on the %s system is not available", runtime.GOOS)
 }
