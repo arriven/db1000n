636d61bc24ca952a6f803b7881ef9e31e84a7cc8
Bohdan Ivashko <20084245+arriven@users.noreply.github.com>
2022-08-08 17:11:11 +0300

    Update install.sh for armv6 (#562)
    * Update install.sh
    
    * Update install.sh
    
    Co-authored-by: Vasily Korzh <109647583+VasilyKorzh@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -23,9 +23,10 @@ fi
 INSTALL_ARCH="unknown"
 case "$OSARCH" in
   x86_64*)  INSTALL_ARCH="amd64" ;;
-  i386*)    INSTALL_ARCH="386" ;; 
-  armv6l)   INSTALL_ARCH="arm" ;;
-  armv7l)   INSTALL_ARCH="arm" ;;
+  i386*)    INSTALL_ARCH="386" ;;
+  i686*)    INSTALL_ARCH="386" ;;
+  armv6l)   INSTALL_ARCH="armv6" ;;
+  armv7l)   INSTALL_ARCH="armv6" ;;
   arm*)     INSTALL_ARCH="arm64" ;;
   aarch64*) INSTALL_ARCH="arm64" ;;
   *)        echo "unknown: $OSARCH"; exit 1 ;;
